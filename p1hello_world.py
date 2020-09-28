8df3cd0bfe9f9df9440c9a4f4273f8437cc9e132
munishstudy73 <munishstudy73@gmail.com>
2020-09-28 05:15:14 -0700

    second commit

---
+++ p1hello_world.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Author:
+Date:
+Program: Print Hello World
+
+"""
+print("Hello World")
---
+++ p2display_input.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Author:
+Date:
+Program: Ask user input and display on screen
+"""
+
+input01=input("pls input:- ")
+print(input01)
