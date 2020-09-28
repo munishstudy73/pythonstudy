8620b64cdfea5068ee99c33f093a0f5c313a6622
munishstudy73 <munishstudy73@gmail.com>
2020-09-28 05:46:32 -0700

    fouth commit

---
+++ p3text_join.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Author:
+Date:
+Program: Test text join
+
+
+"""
+
+Race = "Elf"
+Gender = "Female"
+print(Gender, Race)
+print("Male" "Gnome") 
---
+++ p4quotes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Author:
+Date:
+Program: Study various quotes
+
+"""
+
+q1 = "double quote"
+q2 = 'single quote'
+q3 = """ triple
+                       quote """
+q4 = " double in 'single quote'"
+
+print(q1)
+print(q2)
+print(q3)
+print(q4)
