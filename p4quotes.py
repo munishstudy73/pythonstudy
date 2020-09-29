8b0e371f0f69f824dcea55fbb7228ca4dc0ac929
munishstudy73 <munishstudy73@gmail.com>
2020-09-28 22:10:06 -0700

    p4quote file change

--- p4quotes.py
+++ p4quotes.py
@@ -11,8 +11,20 @@ q2 = 'single quote'
 q3 = """ triple
                        quote """
 q4 = " double in 'single quote'"
+q5 = " single line on \
+two line"
+
+q6 = """
+******************************
+DOCSTRING
+******************************
+"""
+
+
 
 print(q1)
 print(q2)
 print(q3)
 print(q4)
+print(q5)
+print(q6)
